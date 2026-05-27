436426f7835113647031fd7c9e8124398f232166
Davi Couto <davi.couto@grupoge21.com>
2026-05-27 15:09:20 -0300

    feat: backend e frontend configurados, alertas de erros na pagina e validação

--- backend/src/main/java/com/taskflow/backend/model/Tarefa.java
+++ backend/src/main/java/com/taskflow/backend/model/Tarefa.java
@@ -1,26 +1,31 @@
 package com.taskflow.backend.model;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "tb_tarefas")
-@Data //Lombok
+@Data
 public class Tarefa {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O título da tarefa é obrigatório.")
+    @Size(min = 3, max = 100, message = "O título deve ter entre 3 e 100 caracteres.")
     @Column(nullable = false, length = 100)
     private String titulo;
 
+    @Size(max = 500, message = "A descrição não pode passar de 500 caracteres.")
     @Column(columnDefinition = "TEXT")
     private String descricao;
 
+    @FutureOrPresent(message = "A data de vencimento não pode ser uma data passada.")
     private LocalDate dataVencimento;
 
     @Column(nullable = false)
